d09748e2ea033cccdc4d23198bac69dbc4a4bf3a
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-16 18:36:44 -0500

    Make Node and HashMap classes.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
+class HashMap {
+  constructor() {
+    this.buckets = [];
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i += 1) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+    const newNode = new Node(key, value);
+
+  }
+}
