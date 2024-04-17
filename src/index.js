98adac393f5af5067ef9c291c7e4f333bcbeb254
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-04-16 19:26:23 -0500

    Add set to HashMap class.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 class Node {
-  constructor(value) {
+  constructor(key, value) {
+    this.key = key;
     this.value = value;
     this.next = null;
   }
@@ -7,22 +8,36 @@ class Node {
 
 class HashMap {
   constructor() {
-    this.buckets = [];
+    this.buckets = Array(31).fill(null);
   }
 
   hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
-    for (let i = 0; i < key.length; i += 1) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
     }
-
     return hashCode;
   }
 
   set(key, value) {
+    const index = this.hash(key);
     const newNode = new Node(key, value);
+    let currentNode = this.buckets[index];
 
+    if (!currentNode) {
+      this.buckets[index] = newNode;
+    } else {
+      let prevNode = null;
+      while (currentNode) {
+        if (currentNode.key === key) {
+          currentNode.value = value; // Update the value if the key matches
+          return;
+        }
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      prevNode.next = newNode; // Add new node at the end of the list
+    }
   }
 }
